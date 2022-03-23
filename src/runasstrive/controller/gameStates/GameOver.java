7b8cd5c6d2e9a4a011e2b494e5a7e1cc8aca3d65
lorenz <lorenz.m.heinrich@gmail.com>
2022-03-23 23:56:07 +0100

    add missing gamestate classes

---
+++ src/runasstrive/controller/gameStates/GameOver.java
@@ -0,0 +1,38 @@
+package runasstrive.controller.gameStates;
+
+import runasstrive.io.parameters.Parameter;
+import runasstrive.io.parameters.ParameterBundle;
+import runasstrive.model.RunasStrive;
+
+import java.util.List;
+
+public class GameOver extends GameState{
+
+    protected GameOver(RunasStrive runasStrive) {
+        super(runasStrive);
+    }
+
+    @Override
+    public String getPrompt() {
+        //TODO: implement
+        return null;
+    }
+
+    @Override
+    public String repeatPrompt() {
+        //TODO: implement
+        return null;
+    }
+
+    @Override
+    public boolean execute(ParameterBundle parameterBundle) {
+        //TODO: implement
+        return false;
+    }
+
+    @Override
+    public List<Parameter<?>> getParameters() {
+        //TODO: implement
+        return null;
+    }
+}
