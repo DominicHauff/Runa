e61e9f0edf06fb8743171c21e554afce69ad32cc
lorenz <lorenz.m.heinrich@gmail.com>
2022-03-28 03:57:36 +0200

    comment FightResult

--- src/runasstrive/model/FightResult.java
+++ src/runasstrive/model/FightResult.java
@@ -1,5 +1,35 @@
 package runasstrive.model;
 
+/**
+ * An enum defining all possible results of a fight interaction between runa and monsters.
+ *
+ * @author ugget
+ * @version 1.0
+ */
 public enum FightResult {
-    GAME_OVER, GAME_WON, STAGE_CLEARED, LEVEL_CLEARED, CONTINUE
+
+    /**
+     * The game ends, runa is dead
+     */
+    GAME_OVER,
+
+    /**
+     * The game ends, runa wins
+     */
+    GAME_WON,
+
+    /**
+     * Runa has cleared a stage and can advance to the next stage
+     */
+    STAGE_CLEARED,
+
+    /**
+     * Runa has defeated a boss monster and therefore cleared a whole level
+     */
+    LEVEL_CLEARED,
+
+    /**
+     * Nothing special happened, runa need to continue fighting the monsters of the current stage
+     */
+    CONTINUE
 }
